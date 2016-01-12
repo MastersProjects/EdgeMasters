61c948de76846684af5bd09453830005b4c8defc
zperee <elia.perenzin@bluewin.ch>
2016-01-12 10:23:32 +0100

    Signed-off-by: zperee <elia.perenzin@bluewin.ch>

---
+++ src/rmi/Message.java
@@ -0,0 +1,51 @@
+package rmi;
+
+import java.io.Serializable;
+import java.security.Timestamp;
+
+public class Message implements Serializable {
+
+    private static final long serialVersionUID = -1724444053149327065L;
+
+    private User sender;
+    private String msg;
+    private Timestamp time;
+
+    public Message(User sender, Timestamp ts, String msg) {
+        this.setSender(sender);
+        this.setTime(ts);
+        this.setMsg(msg);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "sender=" + sender +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+}
